cae76c70f605afa245e78a3939ce3fb2c07064fd
Ivan Opacak <sandrostross310@gmail.com>
2021-12-23 15:15:56 +0100

    Routing hinzugefügt + erste Route requiert

--- Main/_PlayMoon-Backend/main.js
+++ Main/_PlayMoon-Backend/main.js
@@ -1,19 +1,35 @@
-var express = require("express")
+var express = require('express')
 var app = express()
 
-var dotenv = require("dotenv").config({
-    path: "environment/.env"
+var dotenv = require('dotenv').config({
+    path: 'environment/.env'
 })
-var bodyParser = require("body-parser")
-var cookieParser = require("cookie-parser")
-var cors = require("cors")({
-    origin: "*"
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var cors = require('cors')({
+    origin: '*'
 })
 
-app.use(cors)
-app.use(bodyParser("t"))
-app.use(cookieParser("t"))
+//ROUTES--START
+
+var routeRegister = require('./routing/register')
+
+//ROUTES--END
+
+//--API--START
+
+var db = require('./api/db')
+
+//--API--END--
+
+app.use(routeRegister)
+
+app.use(cors);
+app.use(bodyParser('t'))
+app.use(cookieParser('t'))
 
 var PORT = process.env.PORT | 5000
 
 app.listen(PORT, () => console.log(`--Backend running on port: ${PORT}--`))
+
+app.use()
