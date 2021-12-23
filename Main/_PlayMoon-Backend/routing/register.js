018469e13358ad827dfa96674a273f44a28b7974
Ivan Opacak <sandrostross310@gmail.com>
2021-12-23 15:16:45 +0100

    Die erste Route /auth/register im Backend hinzugefügt (Noch nicht fertig)

---
+++ Main/_PlayMoon-Backend/routing/register.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = express.Router()
+
+//Routing via express in mehrern dateien zu einer main.js zusammengestellt
+
+//Post request:
+router.post('/auth/register', (req, res) => {
+    //Cooming soon
+})
+
+module.exports = router
